d5b7d055f98d4cbac3724cb58b150b536c0662f9
Mental Jokers <mentaljokers@gmail.com>
2023-01-03 16:40:30 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author "mentaljokers"
+description "Free and open sourced crossshair"
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'index.html'
+
+files {
+    'crosshair.png',
+    'index.html',
+    'script.js',
+    'style.css'
+}
+
+lua54 'yes'
